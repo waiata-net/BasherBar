499643cc6856208da6dcf7663dbe6acbea569900
Waiata <97664166+waiata-net@users.noreply.github.com>
2024-04-01 10:34:32 +0800

    Initial Commit

---
+++ BasherBar/BasherBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasherBarApp.swift
+//  BasherBar
+//
+//  Created by Neal Watkins on 2024/4/1.
+//
+
+import SwiftUI
+
+@main
+struct BasherBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
